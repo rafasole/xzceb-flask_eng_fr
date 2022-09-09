897e61ab06a7ae642743764337062eba1d4f3463
Rafael Franco <104632219+rafasole@users.noreply.github.com>
2022-09-09 11:48:35 -0300

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -21,4 +21,5 @@ def renderIndexPage():
     # Write the code to render template
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    #app.run(debug = True, port = 5000)
+    app.run(host = "0.0.0.0", port = 8080)
